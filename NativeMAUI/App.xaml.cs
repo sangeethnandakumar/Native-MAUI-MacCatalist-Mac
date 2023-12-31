50ec3cceafc267650d5a1cd65e91bcf0cbc43982
Sangeeth Nandakumar <twileloop@outlook.com>
2023-12-31 16:11:01 +0530

    Add project files.

---
+++ NativeMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NativeMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NativeMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NativeMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
